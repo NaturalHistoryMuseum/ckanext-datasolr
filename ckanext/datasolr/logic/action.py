70f49cba73317532237bc701791159069e06778b
Alice Heaton <aliceheaton75@gmail.com>
2015-03-16 15:05:54 +0000

    Refactor database calls into a separate class to make it easier to unit-test datastore_solr_search, and add unit tests for datastore_solr_search.

--- ckanext/datasolr/logic/action.py
+++ ckanext/datasolr/logic/action.py
@@ -1,6 +1,7 @@
 import ckan.logic as logic
 import pylons
 
+from ckanext.datasolr.lib.database import Connection
 from ckanext.datasolr.lib.datastore_solr_search import DatastoreSolrSearch
 from ckanext.datastore import db
 
@@ -12,11 +13,12 @@ def datastore_solr_search(context, data_dict):
     This is an alternative to datastore_search, and conforms to the same
     API. See datastore_search.
     """
-    connection = db._get_engine({
+    sqa_cnx = db._get_engine({
         'connection_url': pylons.config['ckan.datastore.write_url']
     }).connect()
+    connection = Connection(sqa_cnx)
     try:
         searcher = DatastoreSolrSearch(context, data_dict, connection)
         return searcher.fetch()
     finally:
-        connection.close()
+        sqa_cnx.close()
