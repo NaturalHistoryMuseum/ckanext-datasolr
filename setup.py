f60b39a113b7f5b752073a1160b40c268d3392e7
benscott <ben@benscott.co.uk>
2016-12-12 16:27:11 +0000

    Add reqs to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,10 @@ setup(
     description='Ckan extension to use Solr to perform datastore queries',
     url='http://github.com/NaturalHistoryMuseum/ckanext-datasolr',
     packages=find_packages(exclude='tests'),
+    install_requires=[
+        'ujson',
+        'sqlparse'
+    ],
 	entry_points="""
         [ckan.plugins]
             datasolr = ckanext.datasolr.plugin:DataSolrPlugin
